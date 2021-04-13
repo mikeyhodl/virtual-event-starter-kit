926d0151670d39205700b789a8bffb9af051e51f
Mike Owino <mike@mikeowino.com>
2021-04-13 16:02:40 +0000

    # To test GitHub OAuth locally

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,32 +1,16 @@
-/**
- * Copyright 2020 Vercel Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://virtual-event-starter-kit.mikeowino.repl.co';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'mikeeowino';
+export const BRAND_NAME = 'WeKnow';
+export const SITE_NAME_MULTILINE = ['WeKnow', 'Conf'];
+export const SITE_NAME = 'WeKnow Conf';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const DATE = 'May 27, 2021';
+export const SHORT_DATE = 'May 27 - 9:00am EAT';
+export const FULL_DATE = 'May 27th 9am East African Time (GMT+3)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
